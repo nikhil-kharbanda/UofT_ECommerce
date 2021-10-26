39cbeb0c441b111352775c6a9301b241d53b78ef
Nikhil Kharbanda <nikhil_kharbanda@hotmail.com>
2021-10-26 00:45:37 -0400

    init commit

---
+++ models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    //define column ID
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //define column category_name
+    category_name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
